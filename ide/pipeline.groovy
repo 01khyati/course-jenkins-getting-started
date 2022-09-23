ef69a5f77528863c18621028febdc4dffb704099
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-09-23 14:23:27 +0530

    pipelineupdate2

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                bat "mvn clean compile"
+                bat "mvn clean package"
                 //-Dmaven.test.failure.ignore=true
             }
         
